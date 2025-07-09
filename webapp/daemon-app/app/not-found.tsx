e7a954e675dca7ffd288f75e3c069cf2e7330b4a
George Haddad <george-haddad@users.noreply.github.com>
2025-07-09 23:45:44 +0200

    feat: initial webapp with docker deploy

---
+++ webapp/daemon-app/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+    return (
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:Arial]">
+            <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+                <div>The daemon cannot find the page, 404</div>
+            </main>
+        </div>
+    );
+}
