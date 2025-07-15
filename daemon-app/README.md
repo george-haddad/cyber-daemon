b14dc5ea23bcf14284c6beccd926e4e266fd77a0
George Haddad <george-haddad@users.noreply.github.com>
2025-07-16 01:16:14 +0200

    chore: remove nextjs and use vite

--- daemon-app/README.md
+++ daemon-app/README.md
@@ -2,7 +2,7 @@
 
 ## Install
 
-```bash
+```shell script
 npm run build
 docker build -t cyber-daemon-app .
 docker run -p 3000:3000 cyber-daemon-app
---
+++ daemon-app/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import demonLogo from './assets/devil.png'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://www.flaticon.com/free-icons/devil" target="_blank">
+          <img src={demonLogo} className="logo" alt="Demon logo" />
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default App
